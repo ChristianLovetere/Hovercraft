cd66ab06fce605ca2112e40a30eb92be580dbd24
Christian Lovetere <christianlovetere@usf.edu>
2025-01-16 15:14:39 -0500

    Code Cleanup and Commenting

--- Hovercraft.h
+++ Hovercraft.h
@@ -1,38 +1,41 @@
+//Header file with definitions for the circular buffer data structure.
+
 #ifndef HOVERCRAFT_H
 #define HOVERCRAFT_H
 
-#define BUFFERSIZE 10
-class circBuffer {
+#define BUFSIZE 10 //size of the buffer. Larger buffer = more delay, less variance
+#define PWM_OFF 1000 //PWM value to indicate no movement for the motors
+class circBuf {
   public:
-    int buf[BUFFERSIZE] = {1000};
+    int buf[BUFSIZE] = {PWM_OFF}; //buffer starts off indicating stationary
     byte iter;
 
-    circBuffer(){
+    circBuf(){
       iter = 0;
     }
 
-    void append(int num){
+    void append(int num){ //add a new value to the buffer and overwrite the oldest
       buf[iter] = num;
-      iterUp();
+      _iterUp();
     }
 
-    void printBuf(){
+    void printBuf(){ //print the entire buffer to the serial terminal for debugging
       String p;
-      for(int i = 0; i < BUFFERSIZE; i++){
+      for(int i = 0; i < BUFSIZE; i++){
         p += String(buf[i]); //append an entry to the string
-        if(i != BUFFERSIZE -1) //no comma after last entry
+        if(i != BUFSIZE -1) //no comma after last entry
           p += ", "; //append a comma and space to the string
       }
       Serial.println(p);
     }
 
     int getBufSize(){
-      return BUFFERSIZE;
+      return BUFSIZE;
     }
 
   private:
-    void iterUp(){
-      if (iter >= BUFFERSIZE-1) //if over buf size, go back to 0th index
+    void _iterUp(){ //increase the iterator, set back to 0 if at final entry
+      if (iter >= BUFSIZE-1)
         iter = 0;
       else
         iter++;
--- Hovercraft.ino
+++ Hovercraft.ino
@@ -1,52 +1,77 @@
+//Communicates with a hovercraft. Writes PWM values between 1000 and 2000 to ESCs to control drone motors. the Lift motor fills the bag on the bottom of the hovercraft. The left and right
+//motors are for forward movement. Of course, powering the left more than right causes a right turn and vice versa. This code expects values from the analog sticks of an NX6 drone controller.
+//As such, the range of microsecond values from the receiver is typically about 1100 to 2000 us. This implementation assumes the arduino is not being used directly for RX and instead is wired
+//to the receiver used with the controller.
+
 #include <Servo.h>
 #include "Hovercraft.h"
+
+#define LIFT_US_MIN 1090
+#define LIFT_US_MAX 1860
+#define LR_US_MIN 1500
+#define LR_US_MAX 1900
+#define ESC_PWM_MIN 1000
+#define ESC_PWM_MAX 2000
+#define NUM_TIERS 4
+
+//#define DEBUG //comment me out to disable debug output to Serial monitor.
+
 ;
 
-//pwm: 3,5,6,9,10,11
+//UNO pwm pins: 3,5,6,9,10,11
 const byte liftChannelPin = 2;
 const byte leftChannelPin = 4;
 const byte rightChannelPin = 7;
 
-const byte liftPWMPin = 3;
-const byte leftPWMPin = 5;
-const byte rightPWMPin = 6;
+const byte liftPWMPin = 3; //lift ESC connected to pin 3 as signal
+const byte leftPWMPin = 5; //left ESC connected to pin 5 as signal
+const byte rightPWMPin = 6; //right ESC connected to pin 6 as signal
 
 Servo liftEsc, leftEsc, rightEsc;
+circBuf liftBuf, leftBuf, rightBuf;
 
+//vars used for printing info to terminal
+#ifdef DEBUG
 unsigned long previousMillis;
 const unsigned long serialInterval = 2000;
+#endif //DEBUG
 
-circBuffer liftBuf, leftBuf, rightBuf;
-
+//initialize pins, Serial, ESCs
 void setup() {
   pinMode(liftChannelPin, INPUT);
   pinMode(leftChannelPin, INPUT);
   pinMode(rightChannelPin, INPUT);
-  
+
+  #ifdef DEBUG
   Serial.begin(9600);
+  #endif //DEBUG
   
   liftEsc.attach(liftPWMPin); //attach lift ESC to PWM pin 3
   leftEsc.attach(leftPWMPin); //attach left ESC to PWM pin 5
   rightEsc.attach(rightPWMPin); //attach right ESC to PWM pin 6
 }
 
+//loop to determine current values requested fom controller and send them to ESCs
 void loop() {
    
   liftBuf.append(pulseIn(liftChannelPin, HIGH));
   leftBuf.append(pulseIn(leftChannelPin, HIGH));
   rightBuf.append(pulseIn(rightChannelPin, HIGH));
   
-  int liftChannelPulse = avgArr(liftBuf.buf, liftBuf.getBufSize());
+  int liftChannelPulse = avgArr(liftBuf.buf, liftBuf.getBufSize()); //pulses are the average of the entire circular buffer's entries
   int rightChannelPulse = avgArr(rightBuf.buf, rightBuf.getBufSize());
   int leftChannelPulse = avgArr(leftBuf.buf, leftBuf.getBufSize());
 
-  //int liftPWM = tieredMap(liftChannelPulse, 1090, 1860, 1000, 2000); //digital conversion
-  int leftPWM = tieredMap(leftChannelPulse, 1500, 1900, 1000, 1600, 4);
-  int rightPWM = tieredMap(rightChannelPulse, 1500, 1900, 1000, 1600, 4);
-  
-  int liftPWM = map(liftChannelPulse, 1090, 1860, 1000, 2000); // analog conversion 
-  //int leftPWM = map(leftChannelPulse, 1500, 1900, 1000, 1400);
-  //int rightPWM = map(rightChannelPulse, 1500, 1900, 1000, 1400); 
+  //conversions here: convert from controller uS to PWM. uncomment one of each to choose tiered values or fully gradual
+  //TIERED:
+  //int liftPWM = tieredMap(liftChannelPulse, LIFT_US_MIN, LIFT_US_MAX, ESC_PWM_MIN, ESC_PWM_MAX, NUM_TIERS);
+  int leftPWM = tieredMap(leftChannelPulse, LR_US_MIN, LR_US_MAX, ESC_PWM_MIN, ESC_PWM_MAX, NUM_TIERS);
+  int rightPWM = tieredMap(rightChannelPulse, LR_US_MIN, LR_US_MAX, ESC_PWM_MIN, ESC_PWM_MAX, NUM_TIERS);
+
+  //GRADUAL (ANALOG):
+  int liftPWM = map(liftChannelPulse, LIFT_US_MIN, LIFT_US_MAX, ESC_PWM_MIN, ESC_PWM_MAX);
+  //int leftPWM = map(leftChannelPulse, LR_US_MIN, LR_US_MAX, ESC_PWM_MIN, ESC_PWM_MAX);
+  //int rightPWM = map(rightChannelPulse, LR_US_MIN, LR_US_MAX, ESC_PWM_MIN, ESC_PWM_MAX); 
 
   normalizePWM(liftChannelPulse, &liftPWM);
   normalizePWM(leftChannelPulse, &leftPWM);
@@ -56,6 +81,8 @@ void loop() {
   leftEsc.writeMicroseconds(leftPWM);
   rightEsc.writeMicroseconds(rightPWM);
 
+  //begin Serial output for debugging values and conversions:
+  #ifdef DEBUG
   unsigned long currentMillis = millis();
   
   if (currentMillis - previousMillis >= serialInterval) {
@@ -82,10 +109,13 @@ void loop() {
     Serial.println(leftPWM - rightPWM);
     Serial.println("-----------------------------------------------------------------");
   }
+  #endif //DEBUG
+  //end Serial output for debugging
   
   delay(10);
 }
 
+//if mapping from uS to PWM leads to a number below min or above max, set to min or max respectively
 void normalizePWM(int channelPulse, int * PWM){
   if (channelPulse == 0)
     *PWM = 0;
@@ -95,6 +125,7 @@ void normalizePWM(int channelPulse, int * PWM){
     *PWM = 1000;
 }
 
+//given an integer array and its size, return the average of all elements in the array
 int avgArr(int* arr, int arrLength){
   int avg = 0;
   for(int i = 0; i < arrLength; i++) //add all values of the array to avg
@@ -103,10 +134,13 @@ int avgArr(int* arr, int arrLength){
   return avg;
 }
 
+//given the value in, its low and high bounds, the low and high bounds of the conversion, and the number of desired tiers, create a tiered conversion.
+//for example: 4 tiers means the first 25% of the range of values is all the same outcome, then from 25 to 50%, and so on
 int tieredMap(int inValue, int inLow, int inHigh, int outLow, int outHigh, int numTiers){
 
   int outValue;
   int tiers[numTiers];
+  
   for(int i = 0; i < numTiers; i++){
     tiers[i] = (inLow*(numTiers-i-1) + inHigh*i)  / (numTiers-1);
     if (inValue <= tiers[i]) {
@@ -114,6 +148,5 @@ int tieredMap(int inValue, int inLow, int inHigh, int outLow, int outHigh, int n
       break;
     }
   }
-
   return outValue;
 }
